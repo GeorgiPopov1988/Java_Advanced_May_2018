c52d36fd81e95e48374b4ba05b74fc716f5c1ed9
Georgi Popov <georgipopov88@gmail.com>
2018-05-14 23:20:16 +0300

    Intro to Java is Done!

---
+++ Java_Advanced_May_2018/src/Intro_to_Java/lab/Modify_a_bit.java
@@ -0,0 +1,25 @@
+package Intro_to_Java.lab;
+
+import java.util.Scanner;
+
+public class Modify_a_bit {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    
+    int n = scanner.nextInt();
+    int p = scanner.nextInt();
+    int v = scanner.nextInt();
+    int mask;
+    int result = 0;
+    
+    if (v == 0) {
+      mask = ~(1<<p);
+      result = n & mask;
+    } else if (v == 1) {
+      mask = 1 << p;
+      result = n | mask;
+    }
+  
+    System.out.println(result);
+  }
+}
