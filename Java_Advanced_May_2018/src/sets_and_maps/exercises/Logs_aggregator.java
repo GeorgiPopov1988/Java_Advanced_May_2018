a07d72492dada95d70b1da0ed2d35e84e348903e
Georgi Popov <georgipopov88@gmail.com>
2018-05-26 08:49:05 +0300

    Logs aggregator - done.

--- Java_Advanced_May_2018/src/sets_and_maps/exercises/Logs_aggregator.java
+++ Java_Advanced_May_2018/src/sets_and_maps/exercises/Logs_aggregator.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Logos_aggregator {
+public class Logs_aggregator {
   public static void main(String[] args) throws IOException {
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     
